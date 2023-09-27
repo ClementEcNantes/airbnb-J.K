00858797ab14b356a3b11e3dc4de9968385c4f33
Clément Jouin <95017825+ClementEcNantes@users.noreply.github.com>
2023-09-27 14:54:44 +0200

    back

--- airbnb-back/app.js
+++ airbnb-back/app.js
@@ -1,19 +1,30 @@
 const express = require("express");
 const app = express();
 const cors = require('cors');
-//const split = require("split3").verbose();
+const sqlite3 = require("sqlite3").verbose();
 const port = 3000;
-//const db = new sqlite3.Database('accomodation.db');
+const db = new sqlite3.Database('accommodations.db');
 
+app.use(cors()); // Gestion des CORS
 
 app.get('/', (req, res) => {
   res.send('API !');
 });
 
-// app.get('/accomodation', (req, res) => {
-
-// })
+app.get('/accommodations', (req, res) => {
+  // Exécutez une requête SQL pour obtenir les données d'accommodations.db
+  db.all('SELECT * FROM accommodations', (err, rows) => {
+    if (err) {
+      console.error(err.message);
+      res.status(500).json({ error: 'Erreur lors de la récupération des données' });
+      return;
+    }
+    
+    // Renvoyer les données en tant que réponse JSON
+    res.json(rows);
+  });
+});
 
 app.listen(port, () => {
-    console.log(`Serveur écoutant sur le port ${port}`);
+  console.log(`Serveur écoutant sur le port ${port}`);
 });
