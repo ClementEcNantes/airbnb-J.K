016e1c69cee58fb7b6c78af3760704640f7d02e5
Clément Jouin <95017825+ClementEcNantes@users.noreply.github.com>
2023-09-27 15:08:05 +0200

    app get id modification

--- airbnb-back/app.js
+++ airbnb-back/app.js
@@ -25,6 +25,31 @@ app.get('/accommodations', (req, res) => {
   });
 });
 
+app.get('/accommodations/:id', (req, res) => {
+    const id = req.params.id; // Récupérer l'ID de la demande
+  
+    // Exécutez une requête SQL pour obtenir une seule ligne en fonction de l'ID
+    db.get('SELECT * FROM accommodations WHERE id = ?', [id], (err, row) => {
+      if (err) {
+        console.error(err.message);
+        res.status(500).json({ error: 'Erreur lors de la récupération des données' });
+        return;
+      }
+  
+      // Vérifiez si une ligne avec l'ID spécifié a été trouvée
+      if (!row) {
+        res.status(404).json({ error: 'Aucun hébergement trouvé avec cet ID' });
+        return;
+      }
+  
+      // Renvoyer la ligne trouvée en tant que réponse JSON
+      res.json(row);
+    });
+  });
+  
+  
+
+
 app.listen(port, () => {
   console.log(`Serveur écoutant sur le port ${port}`);
 });
